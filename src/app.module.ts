98cfa4d75277f074da24935566d3a358e881a1c6
sajanm <sjnmishra8@gmail.com>
2025-04-25 10:04:18 +0530

    setup ConfigModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,16 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { ContactsModule } from './contacts/contacts.module';
 import { BulkActionModule } from './bulk-action/bulk-action.module';
 
 @Module({
-  imports: [BulkActionModule, ContactsModule],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    BulkActionModule,
+    ContactsModule,
+  ],
   controllers: [],
   providers: [],
 })
