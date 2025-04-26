5c3547180e9a5f4b51f4739dd9c293bbbc2f287f
sajanm <sjnmishra8@gmail.com>
2025-04-26 18:02:15 +0530

    integrate typeorm

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,27 @@
 import { Module } from '@nestjs/common';
-import { ConfigModule } from '@nestjs/config';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { ContactsModule } from './contacts/contacts.module';
 import { BulkActionModule } from './bulk-action/bulk-action.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
+    }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        type: 'postgres',
+        database: configService.get('DB_NAME'),
+        host: configService.get('DB_HOST'),
+        port: configService.get('DB_PORT'),
+        username: configService.get('DB_USER'),
+        password: configService.get('DB_PASSWORD'),
+        synchronize: configService.get('DB_SYNC') === 'true',
+        entities: [],
+      }),
     }),
     BulkActionModule,
     ContactsModule,
