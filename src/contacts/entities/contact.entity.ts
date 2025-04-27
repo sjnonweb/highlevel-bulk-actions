9ed4ffd2410b6d628bc64b2b26c45da083d2e0c7
sajanm <sjnmishra8@gmail.com>
2025-04-28 01:21:09 +0530

    add Contact entity schema

---
+++ src/contacts/entities/contact.entity.ts
@@ -0,0 +1,23 @@
+import {
+  Entity,
+  Column,
+  Index,
+} from 'typeorm';
+import { BaseEntity } from 'src/common/entities/base.entity';
+
+@Entity()
+export class Contact extends BaseEntity {
+  @Column()
+  @Index()
+  accountId: string;
+
+  @Column()
+  @Index()
+  email: string;
+
+  @Column()
+  name: string;
+
+  @Column()
+  company: string;
+}
