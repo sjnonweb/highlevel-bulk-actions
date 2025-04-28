3dd1952760ef64d9d521c7d783767723d695b804
sajanm <sjnmishra8@gmail.com>
2025-04-28 13:32:39 +0530

    add BulkActionBatch and BulkActionItem entity

---
+++ src/bulk-action/entities/bulk-action-items.entity.ts
@@ -0,0 +1,28 @@
+import {
+  Entity,
+  Column,
+  ManyToOne,
+} from 'typeorm';
+import { BulkActionItemStatus } from 'src/common/enums/bulk-action-item-status.enum';
+import { BulkAction } from './bulk-action.entity';
+import { BulkActionBatch } from './bulk-action-batch.entity';
+import { BaseEntity } from 'src/common/entities/base.entity';
+
+@Entity()
+export class BulkActionItem extends BaseEntity {
+  @Column({
+    type: 'enum',
+    enum: BulkActionItemStatus,
+    default: BulkActionItemStatus.QUEUED,
+  })
+  status: BulkActionItemStatus
+
+  @Column()
+  entityId: string;
+
+  @ManyToOne(() => BulkAction, bulkAction => bulkAction.items)
+  bulkAction: BulkAction;
+
+  @ManyToOne(() => BulkActionBatch, batch => batch.items)
+  batch: BulkActionBatch;
+}
