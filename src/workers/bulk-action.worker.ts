461bc8710a4afa47ae9b8434c7963acaf78af4cb
sajanm <sjnmishra8@gmail.com>
2025-05-02 13:19:29 +0530

    update status of bulk-action while processing

--- src/workers/bulk-action.worker.ts
+++ src/workers/bulk-action.worker.ts
@@ -2,6 +2,8 @@ import { Processor, WorkerHost } from '@nestjs/bullmq';
 import { Job } from 'bullmq';
 import { Logger } from '@nestjs/common';
 import { BulkActionService } from 'src/bulk-action/bulk-action.service';
+import { BulkActionStatus } from 'src/common/enums/bulk-action-status.enum';
+import { BulkAction } from 'src/bulk-action/entities/bulk-action.entity';
 
 @Processor('bulk-action')
 export class BulkActionWorker extends WorkerHost {
@@ -13,16 +15,25 @@ export class BulkActionWorker extends WorkerHost {
 
   async process(job: Job<any, any, string>): Promise<any> {
     this.logger.log(`Processing job ${job.name} with id ${job.id}`);
+    let bulkAction: BulkAction | undefined;
     try {
       const bulkActionId = job.data.bulkActionId;
-      const bulkAction = await this.bulkActionService.findOne(bulkActionId);
+      bulkAction = await this.bulkActionService.findOne(bulkActionId);
       if (!bulkAction) {
         throw new Error(`Bulk action with id ${bulkActionId} does not exist`);
       }
+      bulkAction.status = BulkActionStatus.IN_PROGRESS;
+      await bulkAction.save();
       await this.bulkActionService.process(bulkAction)
+      bulkAction.status = BulkActionStatus.SUCCESS;
+      await bulkAction.save();
     } catch (error) {
       this.logger.error('An error occured while processing the job');
       this.logger.error(error);
+      if (bulkAction) {
+        bulkAction.status = BulkActionStatus.FAILED;
+        await bulkAction.save();
+      }
     }
     this.logger.log(`Completed job ${job.name} with id ${job.id}`);
     return true;
